59fe76ea7923d89aeaea52bbed964b5593714a91
Mazukiri <bestluxvn2004@gmail.com>
2026-07-07 19:09:25 +0700

    chore: push current codebase state

--- apps/api/src/study-plan/dto/list-study-plan.dto.ts
+++
@@ -1,8 +0,0 @@
-import { STUDY_PLAN_STATUSES } from '@momito/shared';
-import { IsIn, IsOptional } from 'class-validator';
-
-export class ListStudyPlanDto {
-  @IsOptional()
-  @IsIn(STUDY_PLAN_STATUSES)
-  status?: (typeof STUDY_PLAN_STATUSES)[number];
-}
